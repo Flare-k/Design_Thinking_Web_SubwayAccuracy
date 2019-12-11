788b335aa7fdfcf514d40b56e449d1d71d49a2fc
rokkyw@naver.com <rokkyw@naver.com>
2019-12-11 20:10:21 +0900

    Web Implementation.

---
+++ js/test.js
@@ -0,0 +1,9 @@
+const newWindow = document.querySelector(".button-container");
+
+function on() {
+  newWindow.style.display = "block";
+}
+
+function off() {
+  newWindow.style.display = "none";
+}
