889509eb53eddb971a71ef467da013fe76bb9bb2
rokkyw@naver.com <rokkyw@naver.com>
2019-12-18 11:05:50 +0900

    Subway_Accuracy_Line1

--- js/toggle.js
+++ js/toggle.js
@@ -1,17 +1,20 @@
-/* 메인 화면부 버튼, 라벨 */
-const stationBtn = document.querySelectorAll(".station-box__button");
-//const stationLabel = document.querySelectorAll(".station-box__label");
+const stationSpan = document.querySelectorAll(".text");
 
-/* modal 부분 */
+/* modal component */
 const modalContainer = document.querySelector(".modal");
 const modalTitle = modalContainer.querySelector(".title");
 const mainBtn = modalContainer.querySelector(".mainBtn");
+const submitBtn = modalContainer.querySelector(".submitBtn");
 
 /* 역 이름을 받는 임시변수 */
 var tempTitle;
 
 function toggleModal() {
   modalContainer.classList.toggle("show-modal");
+}
+
+function goTimeTable() {
+  location.href = "stationtable.html?station=" + tempTitle;
 }
 
 function windowOnClick(event) {
@@ -21,19 +24,18 @@ function windowOnClick(event) {
 }
 
 function init() {
-  stationBtn.forEach(
-    button =>
-      (button.onclick = function() {
+  stationSpan.forEach(
+    span =>
+      (span.onclick = function() {
         toggleModal();
-        var grandParent = button.parentNode.parentNode;
-        var label = grandParent.querySelector("label");
-        tempTitle = label.innerText;
+        tempTitle = span.innerText;
         modalTitle.innerText = tempTitle;
       })
   );
 }
 
 mainBtn.addEventListener("click", toggleModal);
+submitBtn.addEventListener("click", goTimeTable);
 window.addEventListener("click", windowOnClick);
 
 init();
